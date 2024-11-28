28cc87f35722488a9324516c481b6afe17a39d48
iqra <fhsaleem8104@gmail.com>
2024-11-29 01:15:00 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,13 +16,13 @@ export default function Home() {
       <MenuList />
 
       {/* About Us Section */}
-      <section className="text-center my-8 px-4 md:px-8 lg:px-16">
+      <section className="text-center my-8 px-4 md:px-8 lg:px-16 flex flex-col sm:flex-row sm:flex-wrap sm:justify-between">
         <SectionHeaders subHeader="Our story" mainHeader="About us" />
         <p className="max-w-2xl mx-auto mt-4 text-gray-500 text-base sm:text-lg">
-        The combination of spices and herbs creates a tantalizing aroma that fills the air, making every slice irresistible. Whether you love it bold and spicy or simple and classic, our pizza offers a mouthwatering experience that leaves you craving more.
+          The combination of spices and herbs creates a tantalizing aroma that fills the air, making every slice irresistible. Whether you love it bold and spicy or simple and classic, our pizza offers a mouthwatering experience that leaves you craving more.
         </p>
         <p className="max-w-2xl mx-auto mt-4 text-gray-500 text-base sm:text-lg">
-        Every bite of our pizza is a symphony of flavors that dance on your taste buds. The crispy, golden crust gives way to a rich, tangy tomato sauce, perfectly balanced with a blend of melty, gooey cheese. 
+          Every bite of our pizza is a symphony of flavors that dance on your taste buds. The crispy, golden crust gives way to a rich, tangy tomato sauce, perfectly balanced with a blend of melty, gooey cheese. 
         </p>
       </section>
 
