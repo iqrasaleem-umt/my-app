7ee73004a277729d3fc5361737bbe3dce1009bbf
iqra <fhsaleem8104@gmail.com>
2024-11-29 09:53:52 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,41 +5,43 @@ import MenuList from "./views/menulist";
 
 export default function Home() {
   return (
-    <>
+    <div className="max-w-6xl mx-auto p-4">
       {/* Hero Section */}
       <Hero />
 
       {/* Home Items Section */}
-      <HomeItems />
+      <section className="max-w-6xl mx-auto p-4 sm:px-6 md:px-8 lg:px-16">
+        <HomeItems />
+      </section>
 
-      {/* Menu List Section */}
-      <MenuList />
+      {/* Menu List Section (Hidden on Mobile) */}
+      <section className="  max-w-6xl mx-auto p-4 hidden sm:block px-4 sm:px-6 md:px-8 lg:px-16">
+        <MenuList />
+      </section>
 
       {/* About Us Section */}
-      <section className="text-center my-8 px-4 md:px-8 lg:px-16 flex flex-col sm:flex-row sm:flex-wrap sm:justify-between">
+      <section className="text-center my-6 px-4 sm:my-8 md:my-10">
         <SectionHeaders subHeader="Our story" mainHeader="About us" />
-        <p className="max-w-2xl mx-auto mt-4 text-gray-500 text-base sm:text-lg">
+        <p className="max-w-lg mx-auto mt-4 text-gray-500 text-sm sm:text-base md:text-lg leading-relaxed">
           The combination of spices and herbs creates a tantalizing aroma that fills the air, making every slice irresistible. Whether you love it bold and spicy or simple and classic, our pizza offers a mouthwatering experience that leaves you craving more.
         </p>
-        <p className="max-w-2xl mx-auto mt-4 text-gray-500 text-base sm:text-lg">
-          Every bite of our pizza is a symphony of flavors that dance on your taste buds. The crispy, golden crust gives way to a rich, tangy tomato sauce, perfectly balanced with a blend of melty, gooey cheese. 
+        <p className="max-w-lg mx-auto mt-4 text-gray-500 text-sm sm:text-base md:text-lg leading-relaxed">
+          Every bite of our pizza is a symphony of flavors that dance on your taste buds. The crispy, golden crust gives way to a rich, tangy tomato sauce, perfectly balanced with a blend of melty, gooey cheese.
         </p>
       </section>
 
       {/* Contact Us Section */}
-      <section className="text-center my-8 px-4 md:px-8 lg:px-16">
+      <section className="text-center my-6 px-4 sm:my-8 md:my-10">
         <SectionHeaders subHeader="Don't hesitate" mainHeader="Contact us" />
         <div className="mt-4">
-          <a className="text-xl sm:text-2xl md:text-3xl underline text-gray-500" href="tel:+92:030012345">
+          <a
+            className="text-lg sm:text-xl md:text-2xl underline text-gray-500"
+            href="tel:+92:030012345"
+          >
             +92 0300 12345
           </a>
         </div>
       </section>
-
-      {/* Add to Cart Button Section */}
-      <section className="text-center my-8 px-4 md:px-8 lg:px-16">
-        
-      </section>
-    </>
+    </div>
   );
 }
